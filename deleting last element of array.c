bc25ffe8d571193c6f2792458d91be910e8e5c2b
Sandana07 <choudharisandana@gmail.com>
2025-11-04 22:06:42 +0530

    Create deleting last element of array.c

---
+++ deleting last element of array.c
@@ -0,0 +1,18 @@
+//Delete last element of array
+#include<stdio.h>
+#define cap 10
+int main()
+{   
+    printf("USN : 1SV24CS125\nNAME : Sandana\n");
+    int a[cap]={1,3,5,7,9};
+    int n=5;
+    if(n>0){
+        n--;
+    }
+    else{
+        printf("array is empty");
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
